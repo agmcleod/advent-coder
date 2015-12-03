a035e07b6a9372a913240dd4497163d2379880b5
Aaron McLeod <aaron@uken.com>
2015-12-03 13:00:25 -0500

    setup day two.

---
+++ advent-two/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Result;
+
+fn read_text() -> Result<String> {
+  let mut text = String::new();
+  let mut file = try!(File::open("in.txt"));
+  try!(file.read_to_string(&mut text));
+  Ok(text)
+}
+
+fn main() {
+
+}
