af93cf15f909636e06ffb216d6a2ef138b11875a
Aaron McLeod <aaron@uken.com>
2015-12-03 12:37:57 -0500

    updated solution

--- advent-one/src/main.rs
+++ advent-one/src/main.rs
@@ -1,19 +1,25 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::Result;
 
-fn read_text(text: &mut String) -> std::io::Result<()> {
+fn read_text() -> Result<String> {
+  let mut text = String::new();
   let mut file = try!(File::open("in.txt"));
-  try!(file.read_to_string(text));
-  Ok(())
+  try!(file.read_to_string(&mut text));
+  Ok(text)
 }
 
 fn main() {
-  let mut text = String::new();
-  read_text(&mut text);
-  let v1: Vec<&str> = text.split("(").collect();
-  let v2: Vec<&str> = text.split(")").collect();
-  let went_up = v1.len() - 1;
-  let went_down = v2.len() - 1;
-  let floor = went_up - went_down;
-  println!("On Floor: {:?}", floor);
+  match read_text() {
+    Ok(text) => {
+      let v1: Vec<&str> = text.split("(").collect();
+      let v2: Vec<&str> = text.split(")").collect();
+      let went_up = v1.len() - 1;
+      let went_down = v2.len() - 1;
+      let floor = went_up - went_down;
+      println!("On Floor: {:?}", floor);
+    },
+    Err(reason) => panic!("{:?}", reason),
+  }
+  
 }
