82a8eef6f1f450cd4f7a7462a3945d57ddf33d60
Aaron McLeod <aaron@uken.com>
2015-12-04 17:47:16 -0500

    problem 4 working

--- advent-four/src/main.rs
+++ advent-four/src/main.rs
@@ -4,13 +4,23 @@ use crypto::md5::Md5;
 use crypto::digest::Digest;
 
 fn main() {
-  let input = String::from("iwrupvqb");
+  
   let mut md5 = Md5::new();
 
-  md5.input(input.as_bytes());
-  let mut output = String::new();
-  let mut bytes = output.as_bytes().clone();
-  md5.result(bytes);
-  let my_vec = bytes.iter().cloned().collect();
-  println!("{:?}", String::from_utf8(my_vec).ok().expect("could not parse bytes"));
+  let mut n = 1;
+
+  loop {
+    md5.reset();
+    let mut input = format!("iwrupvqb{:?}", n);
+    md5.input_str(input.as_ref());  
+    let result = md5.result_str();
+
+    if result.starts_with("000000") {
+      break
+    }
+
+    n += 1;
+  }
+
+  println!("{:?}", n);
 }
