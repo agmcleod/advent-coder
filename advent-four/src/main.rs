f2f5a70caf64a51bb3d05407a9f6492e0e1026d9
Aaron McLeod <aaron@uken.com>
2015-12-08 20:44:19 -0500

    whitespace.

--- advent-four/src/main.rs
+++ advent-four/src/main.rs
@@ -4,7 +4,7 @@ use crypto::md5::Md5;
 use crypto::digest::Digest;
 
 fn main() {
-  
+
   let mut md5 = Md5::new();
 
   let mut n = 1;
@@ -12,7 +12,7 @@ fn main() {
   loop {
     md5.reset();
     let mut input = format!("iwrupvqb{:?}", n);
-    md5.input_str(input.as_ref());  
+    md5.input_str(input.as_ref());
     let result = md5.result_str();
 
     if result.starts_with("000000") {
