8c2bcf3975d58e30f8ab79cf3577eea53795befb
Aaron McLeod <aaron.g.mcleod@gmail.com>
2015-12-13 20:10:28 -0500

    problem 7 working.

--- advent-seven/src/main.rs
+++ advent-seven/src/main.rs
@@ -6,7 +6,7 @@ use std::io::Result;
 use std::collections::HashMap;
 use regex::Regex;
 
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 enum Operation {
     Lshift,
     Rshift,
@@ -15,6 +15,7 @@ enum Operation {
     Not
 }
 
+#[derive(Debug)]
 struct Instruction<'a> {
     left: Option<&'a str>,
     operation: Option<&'a Operation>,
@@ -28,11 +29,41 @@ impl<'a> Instruction<'a> {
     }
 }
 
+fn parse_or_get_from_map(num_regex: &Regex, value: &str, wires: &HashMap<&str, u16>) -> Option<u16> {
+    if num_regex.is_match(value) {
+        Some(value.parse().ok().expect("Could not parse"))
+    } else if wires.contains_key(&value) {
+        Some(*wires.get(&value).unwrap())
+    } else {
+        None
+    }
+}
+
 fn read_text() -> Result<String> {
     let mut text = String::new();
     let mut file = try!(File::open("commands.txt"));
     try!(file.read_to_string(&mut text));
     Ok(text)
+}
+
+fn run_operation(left: &u16, right: &u16, operation: &Operation) -> u16 {
+    match operation {
+        &Operation::Lshift => {
+            left << right
+        },
+        &Operation::Rshift => {
+            left >> right
+        },
+        &Operation::And => {
+            left & right
+        },
+        &Operation::Or => {
+            left | right
+        },
+        &Operation::Not => {
+            !right
+        }
+    }
 }
 
 fn main() {
@@ -43,7 +74,7 @@ fn main() {
 
     let mut operation_map: HashMap<&str, Operation> = HashMap::new();
     let mut instructions: Vec<Instruction> = Vec::new();
-    let mut wires: HashMap<&str, i16> = HashMap::new();
+
     operation_map.insert("LSHIFT", Operation::Lshift);
     operation_map.insert("RSHIFT", Operation::Rshift);
     operation_map.insert("AND", Operation::And);
@@ -54,7 +85,7 @@ fn main() {
         if *line == "" {
             continue;
         } else {
-            let words: Vec<&str> = text.split("\n").collect();
+            let words: Vec<&str> = line.split(" ").collect();
             if words.len() == 5 {
                 instructions.push(Instruction::new(
                     Some(words[0]), operation_map.get(words[1]), Some(words[2]), Some(words[4])
@@ -72,7 +103,104 @@ fn main() {
         }
     }
 
-    for instruction in instructions.iter() {
-        
+    let mut wires: HashMap<&str, u16> = HashMap::new();
+    let num_regex = Regex::new(r"\d+").unwrap();
+    let mut processed_lines: Vec<usize> = Vec::new();
+    let mut i = 0;
+    loop {
+        if i >= instructions.len() {
+            i = 0;
+        }
+        if processed_lines.contains(&i) {
+            i += 1;
+            continue
+        }
+        let instruction = instructions.get(i).unwrap();
+        match instruction.left {
+            Some(wire) => {
+                let left: u16 = match parse_or_get_from_map(&num_regex, wire, &wires) {
+                    Some(n) => n,
+                    None => {
+                        i += 1;
+                        continue
+                    }
+                };
+                let mut write_value = 0;
+                let mut wire_to_write: &str = "";
+                {
+                    match instruction.operation {
+                        Some(operation) => {
+                            if instruction.right == None {
+                                panic!("No right {:?}", instruction);
+                            }
+                            let right = instruction.right.unwrap();
+                            let num: u16 = match parse_or_get_from_map(&num_regex, right, &wires) {
+                                Some(n) => n,
+                                None => {
+                                    i += 1;
+                                    continue
+                                }
+                            };
+
+                            write_value = run_operation(&left, &num, &operation);
+                            match instruction.target {
+                                Some(target) => wire_to_write = target,
+                                None => panic!("No target {:?}", instruction)
+                            }
+                        },
+                        None => {
+                            match instruction.target {
+                                Some(target) => {
+                                    wire_to_write = target;
+                                    write_value += left;
+                                },
+                                None => {
+                                    panic!("No target {:?}", instruction)
+                                }
+                            }
+                        }
+                    }
+
+                    if wire_to_write != "" {
+                        wires.insert(wire_to_write, write_value);
+                        processed_lines.push(i);
+                    }
+                }
+            },
+            None => {
+                match instruction.operation {
+                    Some(operation) => {
+                        if instruction.right == None {
+                            panic!("No right {:?}", instruction);
+                        }
+                        let right = instruction.right.unwrap();
+                        let num: u16 = match parse_or_get_from_map(&num_regex, right, &wires) {
+                            Some(n) => n,
+                            None => {
+                                i += 1;
+                                continue
+                            }
+                        };
+                        let temp = 0u16;
+                        let write_value = run_operation(&temp, &num, &operation);
+                        match instruction.target {
+                            Some(target) => {
+                                wires.insert(target, write_value);
+                                processed_lines.push(i);
+                            },
+                            None => panic!("No target {:?}", instruction)
+                        }
+                    },
+                    None => panic!("Cannot find operation for {:?}", instruction)
+                }
+            }
+        };
+        i += 1;
+        if processed_lines.len() == instructions.len() {
+            break;
+        }
+    }
+    if wires.contains_key("a") {
+        println!("{:?}", wires.get("a").unwrap());
     }
 }
