b51d29d5f0f97dc3d47034bd734b0c58c2e91625
Aaron McLeod <aaron.g.mcleod@gmail.com>
2015-12-15 20:17:11 -0500

    8p1 started up.

--- advent-eight/src/main.rs
+++ advent-eight/src/main.rs
@@ -1,6 +1,9 @@
+extern crate regex;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
+use regex::Regex;
 
 fn read_text() -> Result<String> {
     let mut text = String::new();
@@ -16,8 +19,18 @@ fn main() {
     };
 
     let lines: Vec<&str> = text.split("\n").collect();
+    let mut unparsed_count = 0;
+    let mut parsed_count = 0;
+    let regex = Regex::new(r"(\\x[a-f0-9]{2})|(\\\\|\\.)").unwrap();
+    let regex2 = Regex::new(r#"^"|"$"#).unwrap();
     for line in lines.iter() {
-        println!(r##"{}"##, line);
-        println!("{}", line);
+        unparsed_count += line.len();
+        let substituted_text = regex.replace_all(line, ".");
+        let substituted_text = regex2.replace_all(substituted_text.as_ref(), "");
+        println!("{} {}", line, substituted_text);
+        parsed_count += substituted_text.len();
     }
+
+    let amt = unparsed_count - parsed_count;
+    println!("{} {} {}", unparsed_count, parsed_count, amt);
 }
