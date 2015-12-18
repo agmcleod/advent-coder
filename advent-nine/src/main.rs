259a1212bee7d44129f0b3363102dea1d54d19bb
Aaron McLeod <aaron.g.mcleod@gmail.com>
2015-12-17 21:16:42 -0500

    working on a brute force solution.

--- advent-nine/src/main.rs
+++ advent-nine/src/main.rs
@@ -30,18 +30,58 @@ fn parse_lines<'a>(lines: &Vec<&str>) {
 
         if location_distances.contains_key(&root_location) {
             let mut location_collection = location_distances.get_mut(&root_location).unwrap();
-            location_collection.push(Location::new(target_location, distance));
+            location_collection.push(Location::new(String::from(target_location.clone()), distance));
         } else {
             let mut location_collection: Vec<Location> = Vec::new();
-            let location: Location = Location::new(target_location, distance);
+            let location: Location = Location::new(String::from(target_location.clone()), distance);
             location_collection.push(location);
-            location_distances.insert(root_location, location_collection);
+            location_distances.insert(String::from(root_location.clone()), location_collection);
+        }
+
+        if location_distances.contains_key(&target_location) {
+            let mut location_collection = location_distances.get_mut(&target_location).unwrap();
+            location_collection.push(Location::new(root_location, distance));
+        } else {
+            let mut location_collection: Vec<Location> = Vec::new();
+            let location: Location = Location::new(root_location, distance);
+            location_collection.push(location);
+            location_distances.insert(target_location, location_collection);
         }
     }
 
+    let mut distances: Vec<usize> = Vec::new();
+
     for (key, locations) in location_distances.iter() {
-        println!("{:?} {:?}", key, locations);
+        // try each permutation as a start point for this starting location key
+        println!("{:?}", key);
+        for location in locations.iter() {
+            let mut traversed_locations: Vec<String> = Vec::new();
+            let mut distance = location.distance;
+            println!("------{:?}", location.location_key);
+            traversed_locations.push(String::from(key.clone()));
+            traversed_locations.push(String::from(location.location_key.clone()));
+            let mut location_key = &location.location_key;
+            loop {
+                let locations = location_distances.get(location_key).unwrap();
+                for l in locations.iter() {
+                    if !traversed_locations.contains(&l.location_key) {
+                        distance += l.distance;
+                        println!("      -> {:?} + {:?}", l.location_key, l.distance);
+                        traversed_locations.push(String::from(l.location_key.clone()));
+                        location_key = &l.location_key;
+                        break;
+                    }
+                }
+                if traversed_locations.len() == location_distances.keys().len() {
+                    println!("      = {:?}", distance);
+                    distances.push(distance);
+                    break;
+                }
+            }
+        }
     }
+    distances.sort();
+    println!("{:?}", distances[0]);
 }
 
 fn read_text() -> Result<String> {
