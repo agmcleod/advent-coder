d3a1cc3f0506999f0ea9464c0098c50491b8435c
Aaron McLeod <aaron.g.mcleod@gmail.com>
2015-12-29 14:35:05 -0500

    day 17

--- advent-seventeen/src/main.rs
+++ advent-seventeen/src/main.rs
@@ -1,47 +1,74 @@
+extern crate time;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
 
-struct Indexer {
-    current_index: usize,
-    start_index: usize,
-    rollback_index: usize
+const VOLUME_TARGET: usize = 150;
+
+fn add_one_to_flags(use_flags: &mut Vec<usize>) {
+    for flag in use_flags.iter_mut() {
+        if *flag == 1 {
+            *flag = 0;
+        } else if *flag == 0 {
+            *flag = 1;
+            break;
+        } else {
+            panic!("Invalid number: {}", flag);
+        }
+    }
+}
+
+fn generate_power_sets(sizes: &Vec<usize>) -> Vec<Vec<usize>> {
+    let len = sizes.len();
+    let mut use_flags = Vec::<usize>::new();
+    let mut sets: Vec<Vec<usize>> = Vec::new();
+    for i in 0..len {
+        use_flags.push(0);
+    }
+    loop {
+        add_one_to_flags(&mut use_flags);
+        let mut set = Vec::<usize>::new();
+        let mut index = 0;
+        for flag in use_flags.iter() {
+            if *flag == 1 {
+                set.push(index);
+            }
+            index += 1;
+        }
+
+        sets.push(set);
+
+        if !use_flags.contains(&0) {
+            break;
+        }
+    }
+
+    let sets = sets.into_iter().rev().collect::<Vec<_>>();
+
+    sets
 }
 
 fn make_number(value: &str) -> usize {
     value.parse().ok().expect("nope")
 }
 
-fn next_iteration(sizes: &Vec<usize>, indexer: &mut Indexer, unique_count: &mut usize, amount_filled: &mut usize) {
-    indexer.rollback_index += 1;
-    if indexer.rollback_index >= sizes.len() {
-        indexer.start_index += 1;
-        indexer.current_index = indexer.start_index.clone();
-        indexer.rollback_index = indexer.start_index + 1;
-    } else {
-        indexer.current_index = indexer.rollback_index.clone();
-        *amount_filled = sizes[indexer.start_index];
-        next_size(sizes, indexer, unique_count, amount_filled);
-    }
-}
+pub fn process_sizes(sizes: &Vec<usize>) -> usize {
+    let mut unique_count = 0;
 
-fn next_size(sizes: &Vec<usize>, indexer: &mut Indexer, unique_count: &mut usize, amount_filled: &mut usize) {
-    if indexer.current_index == sizes.len() {
-        next_iteration(sizes, indexer, unique_count, amount_filled);
-    } else if *amount_filled + sizes[indexer.current_index] > 150 {
-        indexer.current_index += 1;
-        next_size(&sizes, indexer, unique_count, amount_filled);
-    } else {
-        if *amount_filled + sizes[indexer.current_index] == 150 {
-            *unique_count += 1;
-            indexer.current_index += 1;
-            next_size(&sizes, indexer, unique_count, amount_filled);
-        } else {
-            *amount_filled += sizes[indexer.current_index];
-            indexer.current_index += 1;
-            next_size(&sizes, indexer, unique_count, amount_filled);
+    let mut sets = generate_power_sets(&sizes);
+    sets.sort_by(|a, b| b.len().cmp(&a.len()));
+
+    for set in sets.iter() {
+        if set.len() == 1 {
+            break
+        }
+        if set.iter().fold(0, |acc, &v| acc + sizes[v]) == VOLUME_TARGET {
+            unique_count += 1;
         }
     }
+
+    unique_count
 }
 
 fn read_text() -> Result<String> {
@@ -57,19 +84,8 @@ fn main() {
         Err(e) => panic!("{:?}", e)
     };
 
-    let mut indexer = Indexer{current_index: 0, start_index: 0, rollback_index: 1};
-
     let sizes = text.split("\n").filter(|s| *s != "").map(|s| make_number(s)).collect::<Vec<usize>>();
-    let mut unique_count = 0;
-
-    loop {
-        let mut amount_filled = 0;
-        next_size(&sizes, &mut indexer, &mut unique_count, &mut amount_filled);
-        if indexer.start_index == sizes.len() {
-            break
-        }
-    }
-
-
-    println!("{:?}", unique_count);
+    let start = time::now().to_timespec();
+    println!("{:?}", process_sizes(&sizes));
+    println!("{:?}", time::now().to_timespec() - start);
 }
