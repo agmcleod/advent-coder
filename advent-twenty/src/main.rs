467b3d8f4681205ecb51a34d4b423cb218c78df4
Aaron McLeod <aaron@uken.com>
2016-02-05 09:12:50 -0500

    updated with faster solution.

--- advent-twenty/src/main.rs
+++ advent-twenty/src/main.rs
@@ -1,11 +1,18 @@
 fn apply_presents_to_house(house_num: &usize, min_presents: &usize) -> bool {
-    let mut present_count = 0;
-    for n in 1..(house_num + 1) {
-        if house_num % n == 0 {
-            present_count += n * 10;
+    let sqrt = (*house_num as f32).sqrt() as usize;
+    let result = (2..sqrt + 1).fold(house_num + 1, |acc, i| {
+        if house_num % i == 0 {
+            acc + i + house_num / i
+        } else {
+            acc
         }
-    }
-    present_count >= *min_presents
+    });
+    10 *
+    if sqrt * sqrt == *house_num {
+        result - sqrt
+    } else {
+        result
+    } >= *min_presents
 }
 
 fn main() {
