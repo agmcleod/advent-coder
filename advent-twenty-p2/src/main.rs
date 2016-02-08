a95515d6114521e913db2a507f43ca271c4f9b2b
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-07 22:34:16 -0500

    p2 working

--- advent-twenty-p2/src/main.rs
+++ advent-twenty-p2/src/main.rs
@@ -1,39 +1,28 @@
-fn apply_presents_to_house(house_num: &usize, min_presents: &usize) -> bool {
-    let sqrt = (*house_num as f32).sqrt() as usize;
-    let result = (1..sqrt + 1).fold(0, |acc, i| {
+fn apply_presents_to_house(house_num: &usize, min_presents: &usize, elf_start: &mut usize) -> bool {
+    let result = (*elf_start..*house_num + 1).fold(0, |acc, i| {
         if house_num % i == 0 {
             if house_num / i <= 50 {
-                if i <= 50 {
-                    acc + i + house_num / i
-                } else {
-                    acc + i
-                }
+                acc + i
             } else {
-                if i <= 50 {
-                    acc + house_num / i
-                } else {
-                    acc
-                }
+                *elf_start = i;
+                acc
             }
+
         } else {
             acc
         }
     });
-    11 *
-    if sqrt * sqrt == *house_num {
-        result - sqrt
-    } else {
-        result
-    } >= *min_presents
+    11 * result >= *min_presents
 }
 
 fn main() {
     let min_presents = 29000000;
 
     let mut house_num = 1;
+    let mut elf_start = 1;
 
     loop {
-        if apply_presents_to_house(&house_num, &min_presents) {
+        if apply_presents_to_house(&house_num, &min_presents, &mut elf_start) {
             break
         } else {
             house_num += 1;
