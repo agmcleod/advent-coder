c351e7d69ed7c108058489f5714fab4a516c0d65
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-09 08:51:01 -0500

    working on a correct solution to vary the rings properly.

--- advent-twentyone/src/main.rs
+++ advent-twentyone/src/main.rs
@@ -2,51 +2,130 @@ enum Winner {
     PLAYER, BOSS
 }
 
+#[derive(Debug)]
 struct Item {
     pub cost: usize,
-    pub damage: usize,
-    pub armor: usize
+    pub damage: i32,
+    pub armor: i32
 }
 
 impl Item {
-    pub fn new(cost: usize, damage: usize, armor: usize) -> Item {
+    pub fn new(cost: usize, damage: i32, armor: i32) -> Item {
         Item{ cost: cost, damage: damage, armor: armor}
     }
 }
 
 struct Character {
-    pub hp: usize,
-    damage: usize,
-    armor: usize
+    pub hp: i32,
+    damage: i32,
+    armor: i32
 }
 
 impl Character {
-    pub fn new(hp: usize, damage: usize, armor: usize) -> Character {
+    pub fn new(hp: i32, damage: i32, armor: i32) -> Character {
         Character{ hp: hp, damage: damage, armor: armor}
     }
+}
 
-    pub fn add_armor(self: &mut Character, armor: usize) {
-        self.armor += armor;
-    }
+#[derive(Debug)]
+struct BuyItemState {
+    pub use_armor: bool,
+    pub weapon_index: usize,
+    pub armor_index: usize,
+    pub rings_index_one: usize,
+    pub rings_index_two: usize,
+    pub ring_count: usize,
+    pub done: bool
+}
 
-    pub fn add_damage(self: &mut Character, damage: usize) {
-        self.damage += damage;
-    }
-
-    pub fn reset_stats(self: &mut Character) {
-        self.damage = 0;
-        self.armor = 0;
+impl BuyItemState {
+    pub fn new() -> BuyItemState {
+        BuyItemState{ use_armor: false, weapon_index: 0, armor_index: 0, rings_index_one: 0, rings_index_two: 0, ring_count: 0, done: false }
     }
 }
 
-fn buy_items(weapons: &Vec<Item>, armor: &Vec<Item>, rings: &Vec<Item>) -> Vec<Item> {
-    let items = Vec::<Item>::new();
+fn buy_items<'a>(weapons: &'a Vec<Item>, armor: &'a Vec<Item>, rings: &'a Vec<Item>, buy_item_state: &mut BuyItemState) -> Vec<&'a Item> {
+    let mut items = Vec::<&Item>::new();
 
-    items
+    if buy_item_state.ring_count == 1 && buy_item_state.rings_index_one == rings.len() {
+        buy_item_state.rings_index_one = 0;
+    }
+
+    if buy_item_state.ring_count == 2 && buy_item_state.rings_index_one == rings.len() && buy_item_state.rings_index_two == rings.len() {
+
+    }
+
+    if buy_item_state.use_armor {
+        buy_item_state.armor_index += 1;
+
+        if buy_item_state.armor_index == armor.len() {
+            buy_item_state.armor_index = 0;
+            buy_item_state.weapon_index += 1;
+        }
+    } else {
+        buy_item_state.weapon_index += 1;
+    }
+
+    if buy_item_state.weapon_index == weapons.len() {
+        if buy_item_state.ring_purchase_count < 2 {
+            buy_item_state.ring_purchase_count += 1;
+        } else if !buy_item_state.use_armor {
+            buy_item_state.use_armor = true;
+        } else {
+            buy_item_state.done = true;
+        }
+
+        buy_item_state.armor_index = 0;
+        buy_item_state.weapon_index = 0;
+        buy_item_state.rings_index = 0;
+        items
+    } else {
+        items.push(weapons.get(buy_item_state.weapon_index).unwrap());
+        if buy_item_state.use_armor {
+            items.push(armor.get(buy_item_state.armor_index).unwrap());
+        }
+        if buy_item_state.ring_purchase_count {
+            items.push(rings.get(buy_item_state.rings_index + 1).unwrap());
+        }
+
+        buy_item_state.rings_index += 1;
+
+        items
+    }
 }
 
-fn run_battle(player: &Character, boss: &Character, items: &Vec<Item>) -> Winner {
-    Winner::PLAYER
+fn calculate_damage(attacker_damage: &i32, defender_armor: &i32) -> i32 {
+    let mut damage = attacker_damage - defender_armor;
+    if damage < 1 {
+        damage = 1;
+    }
+
+    damage
+}
+
+fn run_battle(player: &Character, boss: &Character, items: &Vec<&Item>) -> Winner {
+    let mut player_hp = player.hp.clone();
+    let player_damage = items.iter().fold(0, |sum, i| { sum + i.damage });
+    let player_armor = items.iter().fold(0, |sum, i| { sum + i.armor });
+
+    let mut boss_hp = boss.hp.clone();
+    let mut result: Winner;
+
+    loop {
+        boss_hp -= calculate_damage(&player_damage, &boss.armor);
+        if boss_hp > 0 {
+            player_hp -= calculate_damage(&boss.damage, &player_armor);
+            if player_hp <= 0 {
+                result = Winner::BOSS;
+                break;
+            }
+        } else {
+            result = Winner::PLAYER;
+            break;
+        }
+    }
+
+    result
 }
 
 fn sort_by_lowest_cost(items: &mut Vec<Item>) {
@@ -85,20 +164,25 @@ fn main() {
     sort_by_lowest_cost(&mut armor);
     sort_by_lowest_cost(&mut rings);
 
-    let mut player = Character::new(100, 0, 0);
-    let mut cost = 0;
+    let player = Character::new(100, 0, 0);
+    let mut costs = Vec::<usize>::new();
+    let mut buy_item_state = BuyItemState::new();
     loop {
-        let items = buy_items(&weapons, &armor, &rings);
+        let items = buy_items(&weapons, &armor, &rings, &mut buy_item_state);
         match run_battle(&player, &boss, &items) {
             Winner::PLAYER => {
-                cost = items.iter().fold(0, |sum, item| {
+                costs.push(items.iter().fold(0, |sum, item| {
                     sum + item.cost
-                });
-                break
+                }));
             },
-            Winner::BOSS => continue,
+            Winner::BOSS => {},
+        }
+
+        if buy_item_state.done {
+            break
         }
     }
 
-    print!("{:?}", cost);
+    costs.sort();
+    println!("{:?}", costs);
 }
