50a0494904fe59f9fa5577f9a4c166cd1b75fc6b
Aaron McLeod <aaron@uken.com>
2016-02-11 13:21:02 -0500

    spell systems

--- advent-twentytwo/src/main.rs
+++ advent-twentytwo/src/main.rs
@@ -1,11 +1,27 @@
+use std::collections::HashMap;
+
 struct Boss {
-    pub hp: i32,
-    pub damage: i32
+    hp: i32,
+    damage: i32
+}
+
+struct Buff<'a> {
+    buff_name: String,
+    spell_effect: &'a SpellEffect,
+    duration: i32
 }
 
 struct Player {
+    damage: i32,
+    armor: i32,
     hp: i32,
     mana: i32
+}
+
+impl Player {
+    fn new() -> Player {
+        Player{ damage: 0, armor: 0, hp: 50, mana: 500 }
+    }
 }
 
 enum Target {
@@ -13,8 +29,8 @@ enum Target {
 }
 
 struct Spell {
-    pub spell_effects: Vec<SpellEffect>,
-    pub mana_cost: i32
+    spell_effects: Vec<SpellEffect>,
+    mana_cost: i32
 }
 
 impl Spell {
@@ -28,11 +44,11 @@ impl Spell {
 }
 
 struct SpellEffect {
-    pub target: Target,
-    pub damage: i32,
-    pub duration: i32,
-    pub armor: i32,
-    pub mana: i32
+    target: Target,
+    damage: i32,
+    duration: i32,
+    armor: i32,
+    mana: i32
 }
 
 impl SpellEffect {
@@ -45,16 +61,43 @@ impl SpellEffect {
     }
 }
 
+fn apply_buffs<'a>(player: &mut Player, boss: &mut Boss, buffs: &mut Vec<Buff<'a>>) {
+    player.armor = 0;
+    for buff in buffs.iter_mut() {
+        buff.duration -= 1;
+        let spell_effect = &buff.spell_effect;
+        match spell_effect.target {
+            Target::PLAYER => {
+                player.hp += spell_effect.damage;
+                player.armor = spell_effect.armor;
+            },
+            Target::BOSS => {
+
+            }
+        }
+    }
+}
+
+fn choose_spell<'a>(spells: &'a HashMap<String, Spell>, player: &Player, boss: &Boss) -> &'a Spell {
+    if player.mana >= 229 && player.mana <= 229 + 73 {
+        spells.get("recharge").unwrap()
+    } else {
+        spells.get("magic_missle").unwrap()
+    }
+}
+
 fn set_player_start_values(player: &mut Player) {
     player.hp = 50;
     player.mana = 500;
 }
 
+fn take_player_turn(player: &mut Player, boss: &mut Boss, spell: &Spell) {
+
+}
+
 fn main() {
     let boss = Boss{ hp: 71, damage: 10 };
-    let mut player = Player{hp: 0, mana: 0};
-
-    set_player_start_values(&mut player);
+    let mut player = Player::new();
 
     let mut magic_missle = Spell::new(53);
     magic_missle.add_spell_effect(SpellEffect::new(Target::BOSS, -4));
@@ -67,10 +110,15 @@ fn main() {
     shield.add_spell_effect(SpellEffect::new_buff(Target::PLAYER, 0, 6, 7, 0));
 
     let mut poison = Spell::new(173);
-    poison.add_spell_effect(SpellEffect::new_buff(Target::BOSS, 3, 6, 0, 0));
+    poison.add_spell_effect(SpellEffect::new_buff(Target::BOSS, -3, 6, 0, 0));
 
     let mut recharge = Spell::new(229);
     recharge.add_spell_effect(SpellEffect::new_buff(Target::PLAYER, 0, 5, 0, 101));
 
-    let mut spells = vec![magic_missle, drain, shield, poison, recharge];
+    let mut spells = HashMap::<String, Spell>::new();
+    spells.insert(String::from("magic_missle"), magic_missle);
+    spells.insert(String::from("drain"), drain);
+    spells.insert(String::from("shield"), shield);
+    spells.insert(String::from("poision"), poison);
+    spells.insert(String::from("recharge"), recharge);
 }
