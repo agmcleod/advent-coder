56e2bd19b2a5c39bad1f753bc9dadb2ddd44deee
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-14 15:09:48 -0500

    refactored duration onto spell.

--- advent-twentytwo/src/main.rs
+++ advent-twentytwo/src/main.rs
@@ -1,5 +1,6 @@
 extern crate rand;
 
+use std::cmp;
 use std::collections::HashMap;
 use rand::distributions::{IndependentSample, Range};
 
@@ -42,13 +43,14 @@ enum Target {
 #[derive(Debug)]
 struct Spell {
     name: String,
+    duration: i32,
     mana_cost: i32,
     spell_effects: Vec<SpellEffect>
 }
 
 impl Spell {
-    fn new(name: &str, mana_cost: i32) -> Spell {
-        Spell{ name: String::from(name), mana_cost: mana_cost, spell_effects: Vec::<SpellEffect>::new() }
+    fn new(name: &str, mana_cost: i32, duration: i32) -> Spell {
+        Spell{ name: String::from(name), duration: duration, mana_cost: mana_cost, spell_effects: Vec::<SpellEffect>::new() }
     }
 
     fn add_spell_effect(self: &mut Spell, spell_effect: SpellEffect) {
@@ -60,18 +62,17 @@ impl Spell {
 struct SpellEffect {
     target: Target,
     health: i32,
-    duration: i32,
     armor: i32,
     mana: i32
 }
 
 impl SpellEffect {
     fn new(target: Target, health: i32) -> SpellEffect {
-        SpellEffect{ target: target, health: health, duration: 0, armor: 0, mana: 0 }
+        SpellEffect{ target: target, health: health, armor: 0, mana: 0 }
     }
 
-    fn new_buff(target: Target, health: i32, duration: i32, armor: i32, mana: i32) -> SpellEffect {
-        SpellEffect{ target: target, health: health, duration: duration, armor: armor, mana: mana }
+    fn new_buff(target: Target, health: i32, armor: i32, mana: i32) -> SpellEffect {
+        SpellEffect{ target: target, health: health, armor: armor, mana: mana }
     }
 }
 
@@ -114,11 +115,8 @@ fn choose_spell<'a>(spells: &'a HashMap<String, Spell>, spell_keys: &Vec<String>
         spells.get(&spell_keys[0]).unwrap()
     } else {
         let recharge = spells.get("recharge").unwrap();
-        let poison = spells.get("poison").unwrap();
         if player.mana >= recharge.mana_cost && !buff_is_active(buffs, &recharge.name) {
             recharge
-        } else if player.mana >= poison.mana_cost && !buff_is_active(buffs, &poison.name) {
-            poison
         } else {
             let mut rng = rand::thread_rng();
             let mut rand_index;
@@ -146,22 +144,23 @@ fn reset(player: &mut Player, boss: &mut Boss, buffs: &mut Vec<Buff>) {
 fn main() {
     let mut boss = Boss{ hp: 71, damage: 10 };
     let mut player = Player::new();
+    let mut min_hp = 10000;
 
-    let mut magic_missle = Spell::new("magic_missle", 53);
+    let mut magic_missle = Spell::new("magic_missle", 53, 0);
     magic_missle.add_spell_effect(SpellEffect::new(Target::BOSS, -4));
 
-    let mut drain = Spell::new("drain", 73);
+    let mut drain = Spell::new("drain", 73, 0);
     drain.add_spell_effect(SpellEffect::new(Target::BOSS, -2));
     drain.add_spell_effect(SpellEffect::new(Target::PLAYER, 2));
 
-    let mut shield = Spell::new("shield", 113);
-    shield.add_spell_effect(SpellEffect::new_buff(Target::PLAYER, 0, 6, 7, 0));
+    let mut shield = Spell::new("shield", 113, 6);
+    shield.add_spell_effect(SpellEffect::new_buff(Target::PLAYER, 0, 7, 0));
 
-    let mut poison = Spell::new("poison", 173);
-    poison.add_spell_effect(SpellEffect::new_buff(Target::BOSS, -3, 6, 0, 0));
+    let mut poison = Spell::new("poison", 173, 6);
+    poison.add_spell_effect(SpellEffect::new_buff(Target::BOSS, -3, 0, 0));
 
-    let mut recharge = Spell::new("recharge", 229);
-    recharge.add_spell_effect(SpellEffect::new_buff(Target::PLAYER, 0, 5, 0, 101));
+    let mut recharge = Spell::new("recharge", 229, 5);
+    recharge.add_spell_effect(SpellEffect::new_buff(Target::PLAYER, 0, 0, 101));
 
     let mut spells = HashMap::<String, Spell>::new();
     let spell_keys = vec![
@@ -179,10 +178,10 @@ fn main() {
             // start player turn
             apply_buffs(&mut player, &mut boss, &mut buffs);
             let spell = choose_spell(&spells, &spell_keys, &player, &boss, &buffs);
-            for spell_effect in spell.spell_effects.iter() {
-                if spell_effect.duration > 0 {
-                    buffs.push(Buff::new(spell, spell_effect.duration));
-                } else {
+            if spell.duration > 0 {
+                buffs.push(Buff::new(spell, spell.duration));
+            } else {
+                for spell_effect in spell.spell_effects.iter() {
                     apply_spell_effect(&mut player, &mut boss, spell_effect);
                 }
             }
@@ -207,6 +206,11 @@ fn main() {
                 break;
             }
         } else {
+            let t = cmp::min(min_hp, boss.hp);
+            if t < min_hp {
+                min_hp = t;
+                println!("{:?}", min_hp);
+            }
             reset(&mut player, &mut boss, &mut buffs);
             mana = 0;
             continue
