99b4a796bd01a08b7c853cb9894fe4ca19a92d22
Aaron McLeod <aaron@uken.com>
2016-02-17 13:26:47 -0500

    removing from arrays more freely over index tracking

--- advent-twentyfour/src/main.rs
+++ advent-twentyfour/src/main.rs
@@ -2,28 +2,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
 
-fn next_index(containers: &Vec<Vec<usize>>, index: &usize) -> usize {
-    let mut next_i = index - 1;
-
-    loop {
-        let mut index_used = false;
-        for container in containers.iter() {
-            if container.contains(&next_i) {
-                index_used = true;
-                break
-            }
-        }
-
-        if index_used {
-            next_i -= 1;
-        } else {
-            break
-        }
-    }
-
-    next_i
-}
-
 fn read_text() -> Result<String> {
     let mut text = String::new();
     let mut file = try!(File::open("weights.txt"));
@@ -49,9 +27,8 @@ fn main() {
 
     let weight_per_container = weights.iter().fold(0, |sum, &w| sum + w) / 3;
 
-    let mut start_index = weights.len() - 1;
     let mut index = weights.len() - 1;
-    let mut collected_indexes: Vec<usize> = vec![index];
+    let mut collected: Vec<usize> = vec![weights[index]];
     let mut containers: Vec<Vec<usize>> = vec![Vec::new(), Vec::new(), Vec::new()];
 
     let mut current_container_index = 0;
@@ -59,24 +36,27 @@ fn main() {
     let mut weights_copy = weights.clone();
 
     loop {
-        let collected_sum = collected_indexes.iter().map(|i| weights[*i]).fold(0, |sum, w| sum + w);
-        if collected_sum < weight_per_container {
-            index = next_index(&containers, &index);
-            collected_indexes.push(index);
-        } else if collected_sum > weight_per_container {
-            start_index = next_index(&containers, &start_index);
-            index = start_index;
-            collected_indexes.clear();
-            collected_indexes.push(index);
+        let sum = collected.iter().fold(0, |s, &w| { s + w });
+        if sum < weight_per_container {
+            collected.push(weights_copy[index]);
+            weights_copy.remove(index);
+            index = 0;
+        } else if sum > weight_per_container {
+            weights_copy.insert(collected.pop().unwrap(), 0);
+            weights_copy = weights.clone();
+            index += 1;
         } else {
-            for index in collected_indexes.iter() {
-                containers.get_mut(current_container_index).unwrap().push(index.clone());
+            index = 0;
+            let mut container = containers.get_mut(current_container_index).unwrap();
+            for weight in collected.iter() {
+                container.push(weight.clone());
             }
-            collected_indexes.clear();
             current_container_index += 1;
-
             if current_container_index == 2 {
-
+                let sum = weights_copy.iter().fold(0, |s, &w| { s + w });
+                if sum != weight_per_container {
+                    panic!("did not subtract correctly.");
+                }
             }
         }
     }
