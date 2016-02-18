2b7d87a4c2417e74e5d2fcc4a59413be253df4c8
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-17 21:50:23 -0500

    in a better place, but need to start thinking about recursion

--- advent-twentyfour/src/main.rs
+++ advent-twentyfour/src/main.rs
@@ -25,9 +25,10 @@ fn main() {
         weights.push(line.parse().ok().expect("nope"));
     }
 
+    weights.reverse();
     let weight_per_container = weights.iter().fold(0, |sum, &w| sum + w) / 3;
 
-    let mut index = weights.len() - 1;
+    let mut index = 0;
     let mut collected: Vec<usize> = vec![weights[index]];
     let mut containers: Vec<Vec<usize>> = vec![Vec::new(), Vec::new(), Vec::new()];
 
@@ -40,16 +41,22 @@ fn main() {
         if sum < weight_per_container {
             collected.push(weights_copy[index]);
             weights_copy.remove(index);
-            index = 0;
         } else if sum > weight_per_container {
-            weights_copy.insert(collected.pop().unwrap(), 0);
+            if weights_copy.len() == 0 {
+                weights_copy.push(collected.pop().unwrap());
+            } else {
+                weights_copy.insert(0, collected.pop().unwrap());
+            }
+
             weights_copy = weights.clone();
             index += 1;
         } else {
             index = 0;
-            let mut container = containers.get_mut(current_container_index).unwrap();
-            for weight in collected.iter() {
-                container.push(weight.clone());
+            {
+                let mut container = containers.get_mut(current_container_index).unwrap();
+                for weight in collected.iter() {
+                    container.push(weight.clone());
+                }
             }
             current_container_index += 1;
             if current_container_index == 2 {
@@ -57,6 +64,8 @@ fn main() {
                 if sum != weight_per_container {
                     panic!("did not subtract correctly.");
                 }
+            } else {
+                println!("{:?}", containers[0].iter().fold(1, |factor, value| factor * value));
             }
         }
     }
