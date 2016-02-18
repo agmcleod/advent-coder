91d211368676c87d16c2f69ca63d53e39a7ee0e6
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-18 00:35:42 -0500

    working on it

--- advent-twentyfour/src/main.rs
+++ advent-twentyfour/src/main.rs
@@ -29,7 +29,7 @@ fn main() {
     let weight_per_container = weights.iter().fold(0, |sum, &w| sum + w) / 3;
 
     let mut index = 0;
-    let mut collected: Vec<usize> = vec![weights[index]];
+    let mut collected: Vec<usize> = Vec::new();
     let mut containers: Vec<Vec<usize>> = vec![Vec::new(), Vec::new(), Vec::new()];
 
     let mut current_container_index = 0;
@@ -40,7 +40,9 @@ fn main() {
         let sum = collected.iter().fold(0, |s, &w| { s + w });
         if sum < weight_per_container {
             collected.push(weights_copy[index]);
+            println!("{:?}", weights_copy[index]);
             weights_copy.remove(index);
+            println!("{:?}", weights_copy);
         } else if sum > weight_per_container {
             if weights_copy.len() == 0 {
                 weights_copy.push(collected.pop().unwrap());
@@ -58,17 +60,14 @@ fn main() {
                     container.push(weight.clone());
                 }
             }
+            collected.clear();
+            // println!("{:?}", weights_copy);
             current_container_index += 1;
             if current_container_index == 2 {
-                let sum = weights_copy.iter().fold(0, |s, &w| { s + w });
-                if sum != weight_per_container {
-                    panic!("did not subtract correctly.");
-                }
-            } else {
+                println!("{:?}", containers);
                 println!("{:?}", containers[0].iter().fold(1, |factor, value| factor * value));
+                break
             }
         }
     }
-
-    println!("{:?}", weight_per_container);
 }
