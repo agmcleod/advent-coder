5e2700c46e04344779139bdb982a88a7b305ef14
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-02-18 23:56:19 -0500

    thinking through recursion, and reading some suggestions, here's the solution

--- advent-twentyfour/src/main.rs
+++ advent-twentyfour/src/main.rs
@@ -2,11 +2,66 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::Result;
 
+fn comb(arr: &Vec<usize>, n: usize, write_to: &mut Vec<Vec<usize>>) {
+  let mut incl_arr = vec![false; arr.len()];
+  comb_intern(arr, n, &mut incl_arr, 0, write_to);
+}
+
+fn comb_intern(arr: &Vec<usize>, n: usize, incl_arr: &mut Vec<bool>, index: usize, write_to: &mut Vec<Vec<usize>>) {
+  if arr.len() < n + index {
+      return;
+  }
+  if n == 0 {
+    let it = arr.iter().zip(incl_arr.iter()).filter_map(|(val, incl)|
+      if *incl {
+        Some(val)
+      } else {
+        None
+      }
+    ).cloned().collect::<Vec<usize>>();
+
+    write_to.push(it);
+    return;
+  }
+
+  incl_arr[index] = true;
+  comb_intern(arr, n-1, incl_arr, index+1, write_to);
+  incl_arr[index] = false;
+
+  comb_intern(arr, n, incl_arr, index+1, write_to);
+}
+
+fn mul(values: &Vec<usize>) -> usize {
+    values.iter().fold(1, |total, value| total * value)
+}
+
 fn read_text() -> Result<String> {
     let mut text = String::new();
     let mut file = try!(File::open("weights.txt"));
     try!(file.read_to_string(&mut text));
     Ok(text)
+}
+
+fn sum(values: &Vec<usize>) -> usize {
+    values.iter().fold(0, |sum, &v| sum + v)
+}
+
+fn calculate_weights(weights: &Vec<usize>, target: &usize) {
+    for i in 0..weights.len() {
+        let mut out = Vec::<Vec<usize>>::new();
+        comb(&weights, i, &mut out);
+        let mut results = Vec::<usize>::new();
+        for c in out.iter() {
+            if sum(c) == *target {
+                results.push(mul(c));
+            }
+        }
+        if results.len() > 0 {
+            results.sort();
+            println!("{:?}", results.get(0).unwrap());
+            break
+        }
+    }
 }
 
 fn main() {
@@ -25,67 +80,8 @@ fn main() {
         weights.push(line.parse().ok().expect("nope"));
     }
 
-    weights.reverse();
-    let weight_per_container = weights.iter().fold(0, |sum, &w| sum + w) / 3;
+    let target = weights.iter().fold(0, |sum, &w| sum + w) / 3;
+    let target = weights.iter().fold(0, |sum, &w| sum + w) / 4; // part 2
 
-    for i in 0..weights.len() {
-        let mut index = i;
-        let mut collected: Vec<usize> = Vec::new();
-        let mut containers: Vec<Vec<usize>> = vec![Vec::new(), Vec::new(), Vec::new()];
-
-        let mut current_container_index = 0;
-
-        let mut start_index = 0;
-
-        let mut weights_copy = weights.clone();
-
-        loop {
-            let sum = collected.iter().fold(0, |s, &w| { s + w });
-            if sum < weight_per_container {
-                if index >= weights_copy.len() {
-                    start_index += 1;
-                    if start_index >= weights_copy.len() {
-                        break
-                    }
-                    index = start_index;
-                    while collected.len() > 0 {
-                        weights_copy.push(collected.pop().unwrap());
-                    }
-
-                    weights_copy.sort();
-                    weights_copy.reverse();
-                }
-                collected.push(match weights_copy.get(index) {
-                    Some(r) => r.clone(),
-                    None => panic!("could not get weights_copy[{:?}] start_index: {:?}", index, start_index)
-                });
-                weights_copy.remove(index);
-            } else if sum > weight_per_container {
-                if weights_copy.len() == 0 {
-                    weights_copy.push(collected.pop().unwrap());
-                } else {
-                    weights_copy.insert(0, collected.pop().unwrap());
-                }
-                index += 1;
-            } else {
-                index = 0;
-                {
-                    let mut container = containers.get_mut(current_container_index).unwrap();
-                    for weight in collected.iter() {
-                        container.push(weight.clone());
-                    }
-                }
-                collected.clear();
-                // println!("{:?}", weights_copy);
-                current_container_index += 1;
-                if current_container_index == 2 {
-                    containers.sort_by(|a, b| {
-                        a.len().cmp(&b.len())
-                    });
-                    println!("{:?} {:?} {:?}", containers.get(1).unwrap().iter().fold(1, |total, value| total * value), containers.get(1).unwrap().len(), containers.get(2).unwrap().len());
-                    break
-                }
-            }
-        }
-    }
+    calculate_weights(&weights, &target);
 }
